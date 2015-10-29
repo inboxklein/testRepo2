639eea0afe0a20a05b2d1c2caef9c82b1f59ae49
Artjom Klein <artjom@socure.com>
2015-10-28 23:37:13 -0300

    Modeified pca.R

--- pca_tutorial.R
+++ pca_tutorial.R
@@ -4,6 +4,7 @@
 # Principal Components Analysis Using R - P1 http://www.youtube.com/watch?v=5zk93CpKYhg
 # Principal Components Analysis Using R - P2 http://www.youtube.com/watch?v=I5GxNzKLIoU 
 
+# NEW COMMENT
 
 library(calibrate)
 my.classes = read.csv("/Users/artjomk/work/marks.dat")
